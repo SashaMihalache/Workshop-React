c6d01dfe68705b806307a3bf24b8181cf7e5138a
sebastian.mihalache <sasha.mihalache@gmail.com>
2017-09-12 20:05:38 +0300

    Init

---
+++ src/helpers/mockData.js
@@ -0,0 +1,145 @@
+const users = [{
+    "gender": "female",
+    "name": "Heidi Bradley",
+    "address": "7917 e north st",
+    "email": "heidi.bradley@example.com",
+    "phone": "00-2450-5119",
+    "id": 1,
+    "picture": {
+        "large": "https://randomuser.me/api/portraits/women/49.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/49.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/49.jpg"
+    },
+    "nat": "AU",
+    "notes": "Likes dogs but is affraid of cats. It's a rare condition called catphodoglovia."
+}, {
+    "gender": "female",
+    "name": "Maria Popescu",
+    "address": "6563 baisoara",
+    "email": "یسنا.محمدخان@example.com",
+    "phone": "072-83985706",
+    "id": 2,
+    "picture": {
+        "large": "https://randomuser.me/api/portraits/women/0.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/0.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/0.jpg"
+    },
+    "nat": "IR",
+    "notes": "Likes dogs but is affraid of cats. It's a rare condition called catphodoglovia."
+}, {
+    "gender": "male",
+    "name": "Jack Montgomery",
+    "address": "7333 springfield road",
+    "email": "jack.montgomery@example.com",
+    "phone": "061-953-4406",
+    "id": 3,
+    "picture": {
+        "large": "https://randomuser.me/api/portraits/men/76.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/76.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/76.jpg"
+    },
+    "nat": "IE",
+    "notes": "Likes dogs but is affraid of cats. It's a rare condition called catphodoglovia."
+}, {
+    "gender": "female",
+    "name": "Sara Wainio",
+    "address": "9147 bulevardi",
+    "email": "sara.wainio@example.com",
+    "phone": "02-916-528",
+    "cell": "049-828-39-89",
+    "id": 4,
+    "picture": {
+        "large": "https://randomuser.me/api/portraits/women/90.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/90.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/90.jpg"
+    },
+    "nat": "FI",
+    "notes": "Likes dogs but is affraid of cats. It's a rare condition called catphodoglovia."
+}, {
+    "gender": "female",
+    "name": "Pauline Richard",
+    "address": "4374 grande rue",
+    "email": "pauline.richard@example.com",
+    "phone": "(426)-362-3029",
+    "id": 5,
+    "picture": {
+        "large": "https://randomuser.me/api/portraits/women/86.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/86.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/86.jpg"
+    },
+    "nat": "CH",
+    "notes": "Likes dogs but is affraid of cats. It's a rare condition called catphodoglovia."
+}, {
+    "gender": "female",
+    "name": "Anna Robinson",
+    "address": "7371 armagh street",
+    "email": "anna.robinson@example.com",
+    "phone": "(303)-581-7327",
+    "id": 6,
+    "picture": {
+        "large": "https://randomuser.me/api/portraits/women/31.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/31.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/31.jpg"
+    },
+    "nat": "NZ",
+    "notes": "Likes dogs but is affraid of cats. It's a rare condition called catphodoglovia."
+}, {
+    "gender": "female",
+    "name": "Vicenta Bravo",
+    "address": "4755 calle de arturo soria",
+    "email": "vicenta.bravo@example.com",
+    "phone": "932-416-742",
+    "cell": "665-272-063",
+    "id": 7,
+    "picture": {
+        "large": "https://randomuser.me/api/portraits/women/58.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/58.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/58.jpg"
+    },
+    "nat": "ES",
+    "notes": "Likes dogs but is affraid of cats. It's a rare condition called catphodoglovia."
+}, {
+    "gender": "male",
+    "name": "Leoberto Nascimento",
+    "address": "7056 rua espirito santo ",
+    "email": "leoberto.nascimento@example.com",
+    "phone": "(43) 6409-2106",
+    "id": 8,
+    "picture": {
+        "large": "https://randomuser.me/api/portraits/men/79.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/79.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/79.jpg"
+    },
+    "nat": "BR",
+    "notes": "Likes dogs but is affraid of cats. It's a rare condition called catphodoglovia."
+}, {
+    "gender": "male",
+    "name": "Konsta Latt",
+    "address": "2975 tehtaankatu",
+    "email": "konsta.latt@example.com",
+    "phone": "05-028-157",
+    "id": 9,
+    "picture": {
+        "large": "https://randomuser.me/api/portraits/men/93.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/93.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/93.jpg"
+    },
+    "nat": "FI",
+    "notes": "Likes dogs but is affraid of cats. It's a rare condition called catphodoglovia."
+}, {
+    "gender": "female",
+    "name": "Saakje Snepvangers",
+    "address": "8362 donkerstraat",
+    "email": "saakje.snepvangers@example.com",
+    "phone": "(149)-262-4798",
+    "id": 10,
+    "picture": {
+        "large": "https://randomuser.me/api/portraits/women/30.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/30.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/30.jpg"
+    },
+    "nat": "NL",
+    "notes": "Likes dogs but is affraid of cats. It's a rare condition called catphodoglovia."
+}]
+
+export default users;
